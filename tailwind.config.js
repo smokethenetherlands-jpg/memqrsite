c644f96223760609107076e4e50454411c26ab95
Владелец <smokethenetherlands@gmail.com>
2025-08-15 18:49:45 +0300

    chore: add tailwindcss/postcss/autoprefixer and configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: { extend: {} },
   plugins: [],
 }
