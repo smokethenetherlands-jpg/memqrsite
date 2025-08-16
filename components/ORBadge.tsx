4f6977a11cba960ba50ce8960bb2a277ca5ae6c7
Владелец <smokethenetherlands@gmail.com>
2025-08-16 10:39:35 +0300

    fix: add ORBadge component

---
+++ components/ORBadge.tsx
@@ -0,0 +1,17 @@
+export default function ORBadge() {
+  return (
+    <span
+      style={{
+        display: "inline-block",
+        padding: "2px 6px",
+        background: "#eee",
+        borderRadius: "4px",
+        fontSize: "12px",
+        fontWeight: "bold",
+        color: "#333",
+      }}
+    >
+      QR Memory
+    </span>
+  );
+}
